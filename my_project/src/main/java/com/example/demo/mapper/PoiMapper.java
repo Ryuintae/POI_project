fd6c4d15251840f9f13eb20d0afe6789bdb53f07
Ryuintae <horeem0322@gmail.com>
2023-09-01 10:47:50 +0900

    :bug: [fix] 오타 수정
    WHㅌERE 로 잘못 저장되어서 WHERE 로 수정했습니다

--- my_project/src/main/java/com/example/demo/mapper/PoiMapper.java
+++ my_project/src/main/java/com/example/demo/mapper/PoiMapper.java
@@ -14,14 +14,12 @@ public interface PoiMapper {
     Poi findById(int poi_num);
 
     // POI 정보 등록
-    @Insert("INSERT INTO public.poi_data (user_id, category_code, poi_name, iclas, mlsfc, sclas, dclas, tel_no, memo, lon, lat, address, zip_code) " +
-            "VALUES (#{user_id}, #{category_code}, #{poi_name}, #{iclas}, #{mlsfc}, #{sclas}, #{dclas}, #{tel_no}, #{memo}, #{lon}, #{lat}, #{address}, #{zip_code});")
+    @Insert("INSERT INTO public.poi_data (user_id, category_code, poi_name, iclas, mlsfc, sclas, dclas, tel_no, memo, lon, lat, address, zip_code) " + "VALUES (#{user_id}, #{category_code}, #{poi_name}, #{iclas}, #{mlsfc}, #{sclas}, #{dclas}, #{tel_no}, #{memo}, #{lon}, #{lat}, #{address}, #{zip_code});")
     @Options(useGeneratedKeys = true, keyProperty = "poi_num")
     void register(Poi poi);
 
     // POI 정보 수정
-    @Update("UPDATE public.poi_data SET user_id=#{user_id}, category_code=#{category_code}, poi_name=#{poi_name}, iclas=#{iclas}, mlsfc=#{mlsfc}, sclas=#{sclas}, dclas=#{dclas}, tel_no=#{tel_no}, memo=#{memo}, lon=#{lon}, lat=#{lat} " +
-            "WHERE poi_num=#{poi_num};")
+    @Update("UPDATE public.poi_data SET user_id=#{user_id}, category_code=#{category_code}, poi_name=#{poi_name}, iclas=#{iclas}, mlsfc=#{mlsfc}, sclas=#{sclas}, dclas=#{dclas}, tel_no=#{tel_no}, memo=#{memo}, lon=#{lon}, lat=#{lat} " + "WHERE poi_num=#{poi_num};")
     void update(Poi poi);
 
     // POI 정보 삭제
@@ -29,7 +27,7 @@ public interface PoiMapper {
     void delete(int poi_num);
 
     // POI 이름 검색
-    @Select("SELECT * FROM public.poi_data WHㅌERE poi_name like '%${poi_name}%';")
+    @Select("SELECT * FROM public.poi_data WHERE poi_name like '%${poi_name}%';")
     List<Poi> findByName(String poi_name);
 
     // user_id에 해당하는 모든 POI 정보 조회
