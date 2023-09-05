be9d7a470e280c401a36824c5fbbe31b52d4b8ae
Ryuintae <horeem0322@gmail.com>
2023-09-05 16:22:50 +0900

    :fire: 네이버 키 값 비공개 전환
    Service 클래스 에서 고유 key 값이 노출이 되어서 재등록 후 application.yml 폴더에 옮겼습니다.

--- my_project/src/main/java/com/example/demo/service/NaverMapService.java
+++ my_project/src/main/java/com/example/demo/service/NaverMapService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,10 @@ import org.springframework.http.HttpHeaders;
 
 @Service
 public class NaverMapService {
-    private final String NAVER_API_KEY = "yyorj1lmap";
+    @Value("${naver.api.key.id}")
+    private String NAVER_API_KEY_ID;
+    @Value("${naver.api.key.secret}")
+    private String NAVER_API_KEY_SECRET;
 
     public String getDirections(String start, String end) {
         String apiUrl = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving";
@@ -22,8 +26,8 @@ public class NaverMapService {
         RestTemplate restTemplate = new RestTemplate();
         // 네이버 API 요청에 필요한 헤더 설정
         HttpHeaders headers = new HttpHeaders(); // 올바른 HttpHeaders 클래스 사용
-        headers.set("X-NCP-APIGW-API-KEY-ID", NAVER_API_KEY);
-        headers.set("X-NCP-APIGW-API-KEY", "CjJLHilLR4lz3Lmps6XdhkZH0XKyUXVUGXs9bDqJ");
+        headers.set("X-NCP-APIGW-API-KEY-ID", NAVER_API_KEY_ID);
+        headers.set("X-NCP-APIGW-API-KEY", NAVER_API_KEY_SECRET);
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
